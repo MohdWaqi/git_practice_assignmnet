f88f98c78c06bc6cf59b14ece511748580523a2d
Mohd Waqi Pervez <117337520+MohdWaqi@users.noreply.github.com>
2023-08-26 16:50:48 +0530

    Revert "correct palindrome file"

--- palindrome.js
+++ palindrome.js
@@ -1,12 +1,1 @@
-let num= 1221;
-let numberpal = num;
-let reverse = 0;
-while(numberpal > 0){
-    reverse = reverse *10 + numberpal%10;
-    numberpal = Math.floor(numberpal/10);
-}
-if(num === reverse ){
-    console.log("Palindrome"); 
-}else{
-    console.log("Not a Palindrome");
-}
+This is so much fun.
